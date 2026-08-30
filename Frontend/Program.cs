1c4a97706f9a1579460cae2cff4a20aa602f7f86
Sebastian Padilla <sebaz4825@gmail.com>
2026-08-30 09:33:38 -0500

    add rhombus

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -3,10 +3,11 @@
 
 var circle = new Circle(name: nameof(Circle), r: 5);
 var square = new Square(name: nameof(Square), a: 10);
+var rhombus = new Rhombus(name: nameof(Rhombus), a: 5, d1: 7, d2: 10);
 
 var figures = new List<GeometricFigure>
 {
-    circle, square
+    circle, square, rhombus
 };
 
 foreach (var figure in figures)
---
+++ Inheritance/Rhombus.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Backend;
+
+public class Rhombus : Square
+
+{
+    //Fields
+    private double _d1;
+    private double _d2;
+
+
+//Constructor
+public Rhombus(string name, double a, double d1, double d2) : base(name, a)
+    {
+        D1 = d1;
+        D2 = d2;
+    }
+    // Propertier
+    public double D1
+    {
+        get => _d1;
+        set => _d1 = ValidateD1(value);
+    }
+
+    public double D2
+    {
+        get => _d2;
+        set => _d2 = ValidateD2(value);
+    }
+    // Methods
+    public override double GetArea()
+    {
+        return (_d1 * _d2) / 2;
+    }
+    public override double GetPerimiter()
+    {
+        return base.GetPerimiter();
+    }
+    private double ValidateD1(double d1)
+    {
+        if (d1 <= 0)
+        {
+            throw new ArgumentException($"The radius: {d1}, is not valid.");
+        }
+        return d1;
+    }
+    private double ValidateD2(double d2)
+    {
+        if (d2 <= 0)
+        {
+            throw new ArgumentException($"The radius: {d2}, is not valid.");
+        }
+        return d2;
+    }
+}
