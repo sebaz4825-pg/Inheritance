6fbe726e6d4f16ae272739f3504e46cc144efe61
Sebastian Padilla <sebaz4825@gmail.com>
2026-08-30 11:40:40 -0500

    add triangle

---
+++ Inheritance/Triangle.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace Backend;
+
+public class Triangle : Rectangle
+
+{
+    //Fields
+    private double _c;
+    private double _h;
+
+
+    //Constructor
+    public Triangle(string name, double a, double b, double c, double h) : base(name, a,b)
+    {
+        C = c;
+        H = h;
+    }
+    // Propertier
+    public double C
+    {
+        get => _c;
+        set => _c = ValidateC(value);
+    }
+
+    public double H
+    {
+        get => _h;
+        set => _h = ValidateH(value);
+    }
+    // Methods
+    public override double GetArea()
+    {
+        return (B * H) / 2;
+    }
+    public override double GetPerimiter()
+    {
+        return A + B + _c;
+    }
+    private double ValidateC(double c)
+    {
+        if (c <= 0)
+        {
+            throw new ArgumentException($"The sice C: {c}, is not valid.");
+        }
+        return c;
+    }
+    private double ValidateH(double h)
+    {
+        if (h <= 0)
+        {
+            throw new ArgumentException($"The height: {h}, is not valid.");
+        }
+        return h;
+    }
+}
